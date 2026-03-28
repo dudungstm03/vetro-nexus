550e40a33e7addcbeb5a57c8fb02d933a20d507d
dudungstm03 <dudungstm03@gmail.com>
2026-03-28 22:31:11 +0700

    Create MainActivity.kt with Android navigation and Hilt setup

---
+++ app/src/main/java/com/vetro/nexus/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.vetro.nexus
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen(viewModel: MainViewModel = hiltViewModel()) {
+    Surface(color = MaterialTheme.colorScheme.background) {
+        // Navigation setup can be done here
+    }
+}
